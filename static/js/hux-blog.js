74061e447ee0d05445a6a8f4a0202dd18e1b6087
zhaohuabing <zhaohuabing@gmail.com>
2021-09-29 20:32:09 +0800

    support catalog sidebar
    https://github.com/zhaohuabing/hugo-theme-cleanwhite/issues/108
    
    Signed-off-by: zhaohuabing <zhaohuabing@gmail.com>

--- static/js/hux-blog.js
+++ static/js/hux-blog.js
@@ -40,12 +40,14 @@ jQuery(document).ready(function($) {
 
     //primary navigation slide-in effect
     if ($(window).width() > MQL) {
-        var headerHeight = $('.navbar-custom').height();
+            var headerHeight = $('.navbar-custom').height(),
+        bannerHeight = $('.intro-header .container').height();
         $(window).on('scroll', {
                 previousTop: 0
             },
             function() {
-                var currentTop = $(window).scrollTop();
+                var currentTop = $(window).scrollTop(),
+                    $catalog = $('.side-catalog');
                 //check if user is scrolling up
                 if (currentTop < this.previousTop) {
                     //if scrolling up...
@@ -60,6 +62,14 @@ jQuery(document).ready(function($) {
                     if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
                 }
                 this.previousTop = currentTop;
+
+                //adjust the appearance of side-catalog
+                $catalog.show()
+                if (currentTop > (bannerHeight + 41)) {
+                    $catalog.addClass('fixed')
+                } else {
+                    $catalog.removeClass('fixed')
+                }
             });
     }
 });
